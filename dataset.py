1e8bd6d517728aba6726adea606a20354e0fddcc
oza <oza15015@keio.jp>
2020-05-26 18:03:59 +0900

    initial commit

---
+++ dataset.py
@@ -0,0 +1,154 @@
+"""dataset.py"""
+
+import os
+import random
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+from sklearn.datasets import fetch_openml #MNIST
+
+
+def is_power_of_2(num):
+    return ((num & (num - 1)) == 0) and num != 0
+
+i = 0
+class CustomImageFolder(ImageFolder):
+    def __init__(self, root, transform=None):
+        super(CustomImageFolder, self).__init__(root, transform)
+        self.indices = range(len(self))
+
+    def __getitem__(self, index1):
+        index2 = random.choice(self.indices)
+        path1 = self.imgs[index1][0]
+        path2 = self.imgs[index2][0]
+        img1 = self.loader(path1)
+        img2 = self.loader(path2)
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+        return img1, img2
+
+class CustomTensorDataset(Dataset):
+    def __init__(self, data_tensor, transform=None):
+        self.data_tensor = data_tensor
+        self.transform = transform
+        self.indices = range(len(self))
+ 
+    def __getitem__(self, index1):
+        index2 = random.choice(self.indices)
+ 
+        img1 = self.data_tensor[index1]
+        img2 = self.data_tensor[index2] #index1と違くしたいため乱数っぽい
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+ 
+        return img1, img2
+ 
+    def __len__(self):
+        return self.data_tensor.size(0)
+
+
+
+class MNISTDataset(Dataset):
+    def __init__(self, data_tensor=None, transform=None):
+        self.mnist = fetch_openml('mnist_784', version=1,)
+        self.data_tensor = self.mnist.data.reshape(-1, 28, 28).astype('uint8')
+        self.transform = transform
+        self.target = self.mnist.target.astype(int)
+        self.indices = range(len(self))
+
+    def __getitem__(self, index1):
+        index2 = random.choice(self.indices)
+
+        img1 = self.data_tensor[index1]
+        img2 = self.data_tensor[index2] #index1と違くしたいため乱数っぽい
+        target1 = torch.from_numpy(np.array(self.target[index1]))
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+        return img1, img2
+
+    def __len__(self):
+        #return self.data_tensor.size(0)
+        return len(self.data_tensor)
+
+class load_MNISTDataset(Dataset):
+    
+    def __init__(self, data_tensor=None, transform=None):
+        self.mnist = fetch_openml('mnist_784', version=1,)
+        self.data_tensor = self.mnist.data.reshape(-1, 28, 28).astype('uint8')
+        self.target = self.mnist.target.astype(int)
+        self.indices = range(len(self))
+        self.transform = transform
+            
+    def __len__(self):
+        return len(self.data_tensor)
+    
+    def __getitem__(self, idx):
+        idx2 = random.choice(self.indices)
+        data1 = self.data_tensor[idx]
+        #data2 = self.data[idx2] 
+        target1 = torch.from_numpy(np.array(self.target[idx])) #torch.from_numpy()でTensorに変換
+        if self.transform:
+            data1 = self.transform(data1)
+            
+        #sample = data1
+        return data1, target1
+
+
+def return_data(args):
+    name = args.dataset
+    dset_dir = args.dset_dir
+    batch_size = args.batch_size
+    num_workers = args.num_workers
+    image_size = args.image_size
+    assert image_size == 64, 'currently only image size of 64 is supported'
+
+    transform = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),])
+    
+    transform2 = transforms.ToTensor() #MNIST
+
+    if name.lower() == 'celeba':
+        root = os.path.join(dset_dir, 'CelebA')
+        train_kwargs = {'root':root, 'transform':transform}
+        dset = CustomImageFolder
+    elif name.lower() == '3dchairs':
+        root = os.path.join(dset_dir, '3DChairs')
+        train_kwargs = {'root':root, 'transform':transform}
+        dset = CustomImageFolder
+    elif name.lower() == 'dsprites':
+        root = os.path.join(dset_dir, 'dsprites-dataset/dsprites_ndarray_co1sh3sc6or40x32y32_64x64.npz')
+        data = np.load(root, encoding='latin1')
+        data = torch.from_numpy(data['imgs']).unsqueeze(1).float()
+        train_kwargs = {'data_tensor':data}
+        dset = CustomTensorDataset
+    elif name.lower() == 'mnist': #試験的MNIST
+        print("Test_MNIST")
+        train_kwargs = {'data_tensor':None, 'transform':transform2}
+        dset = MNISTDataset
+    elif name.lower() == 'load_mnist': #試験的MNIST
+        print("load_MNIST")
+        train_kwargs = {'data_tensor':None, 'transform':transform2}
+        dset = load_MNISTDataset
+
+    else:
+        raise NotImplementedError
+
+
+    train_data = dset(**train_kwargs)
+    train_loader = DataLoader(train_data,
+                              batch_size=batch_size,
+                              shuffle=True,
+                              num_workers=num_workers,
+                              pin_memory=True,
+                              drop_last=True)
+
+    data_loader = train_loader
+    return data_loader
---
+++ model.py
@@ -0,0 +1,352 @@
+"""model.py"""
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from torch.nn import functional as F
+
+class Discriminator(nn.Module):
+    def __init__(self, z_dim):
+        super(Discriminator, self).__init__()
+        self.z_dim = z_dim
+        self.net = nn.Sequential(
+            nn.Linear(z_dim, 1000),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1000, 1000),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1000, 1000),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1000, 1000),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1000, 1000),
+            nn.LeakyReLU(0.2, True),
+            nn.Linear(1000, 2),
+        )
+        self.weight_init()
+
+    def weight_init(self, mode='normal'):
+        if mode == 'kaiming':
+            initializer = kaiming_init
+        elif mode == 'normal':
+            initializer = normal_init
+
+        for block in self._modules:
+            for m in self._modules[block]:
+                initializer(m)
+
+    def forward(self, z):
+        return self.net(z).squeeze()
+
+
+class FactorVAE1(nn.Module):
+    """Encoder and Decoder architecture for 2D Shapes data."""
+    def __init__(self, z_dim=10):
+        super(FactorVAE1, self).__init__()
+        self.z_dim = z_dim
+        self.encode = nn.Sequential(
+            nn.Conv2d(1, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 128, 4, 1),
+            nn.ReLU(True),
+            nn.Conv2d(128, 2*z_dim, 1)
+        )
+        self.decode = nn.Sequential(
+            nn.Conv2d(z_dim, 128, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 1, 4, 2, 1),
+        )
+        self.weight_init()
+
+    def weight_init(self, mode='normal'):
+        if mode == 'kaiming':
+            initializer = kaiming_init
+        elif mode == 'normal':
+            initializer = normal_init
+
+        for block in self._modules:
+            for m in self._modules[block]:
+                initializer(m)
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = std.data.new(std.size()).normal_()
+        print("eps")
+        print(eps.shape)
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x, no_dec=False):
+        stats = self.encode(x)
+        mu = stats[:, :self.z_dim]
+        logvar = stats[:, self.z_dim:]
+        z = self.reparametrize(mu, logvar)
+
+        if no_dec:
+            return z.squeeze()
+        else:
+            x_recon = self.decode(z).view(x.size())
+            return x_recon, mu, logvar, z.squeeze()
+
+
+class FactorVAE2(nn.Module):
+    """Encoder and Decoder architecture for 3D Shapes, Celeba, Chairs data."""
+    def __init__(self, z_dim=10):
+        super(FactorVAE2, self).__init__()
+        self.z_dim = z_dim
+        self.encode = nn.Sequential(
+            nn.Conv2d(3, 32, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.Conv2d(32, 32, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.Conv2d(32, 64, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.Conv2d(64, 64, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.Conv2d(64, 256, 4, 1),
+            nn.ReLU(inplace = False),
+            nn.Conv2d(256, 2*z_dim, 1)
+        )
+        self.decode = nn.Sequential(
+            nn.Conv2d(z_dim, 256, 1),
+            nn.ReLU(inplace = False),
+            nn.ConvTranspose2d(256, 64, 4),
+            nn.ReLU(inplace = False),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.ConvTranspose2d(32, 32, 4, 2, 1),
+            nn.ReLU(inplace = False),
+            nn.ConvTranspose2d(32, 3, 4, 2, 1),
+        )
+        self.weight_init()
+
+    def weight_init(self, mode='normal'):
+        if mode == 'kaiming':
+            initializer = kaiming_init
+        elif mode == 'normal':
+            initializer = normal_init
+
+        for block in self._modules:
+            for m in self._modules[block]:
+                initializer(m)
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = std.data.new(std.size()).normal_()
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x, no_dec=False):
+        stats = self.encode(x)
+        mu = stats[:, :self.z_dim]
+        logvar = stats[:, self.z_dim:]
+        z = self.reparametrize(mu, logvar)
+
+        if no_dec:
+            return z.squeeze()
+        else:
+            x_recon = self.decode(z)
+            return x_recon, mu, logvar, z.squeeze()
+
+
+class FactorVAE3(nn.Module):
+    """Encoder and Decoder architecture for 3D Faces data."""
+    def __init__(self, z_dim=10):
+        super(FactorVAE3, self).__init__()
+        self.z_dim = z_dim
+        self.encode = nn.Sequential(
+            nn.Conv2d(1, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 256, 4, 1),
+            nn.ReLU(True),
+            nn.Conv2d(256, 2*z_dim, 1)
+        )
+        self.decode = nn.Sequential(
+            nn.Conv2d(z_dim, 256, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 64, 4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 32, 4, 2, 1),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 1, 4, 2, 1),
+        )
+        self.weight_init()
+
+    def weight_init(self, mode='normal'):
+        if mode == 'kaiming':
+            initializer = kaiming_init
+        elif mode == 'normal':
+            initializer = normal_init
+
+        for block in self._modules:
+            for m in self._modules[block]:
+                initializer(m)
+
+    def reparametrize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        eps = std.data.new(std.size()).normal_()
+        return eps.mul(std).add_(mu)
+
+    def forward(self, x, no_dec=False):
+        stats = self.encode(x)
+        mu = stats[:, :self.z_dim]
+        logvar = stats[:, self.z_dim:]
+        z = self.reparametrize(mu, logvar)
+
+        if no_dec:
+            return z.squeeze()
+        else:
+            x_recon = self.decode(z)
+            return x_recon, mu, logvar, z.squeeze()
+
+
+class Custom_FactorVAE1(nn.Module):
+    """Encoder and Decoder architecture for MNIST_Test"""
+    def __init__(self, z_dim=10):
+        super(Custom_FactorVAE1, self).__init__()
+        self.z_dim = z_dim
+        self.encode = nn.Sequential(
+            nn.Linear(28*28, 200),
+            nn.ReLU(True),
+            nn.Linear(200, 100),
+            nn.ReLU(True),
+            nn.Linear(100, z_dim)
+        )
+        self.decode = nn.Sequential(
+            nn.Linear(z_dim, 100),
+            nn.ReLU(True),
+            nn.Linear(100, 200),
+            nn.ReLU(True),
+            nn.Linear(200, 28*28),
+            nn.Sigmoid()
+        )
+        self.weight_init()
+
+    def weight_init(self, mode='normal'):              
+        if mode == 'kaiming':                          
+            initializer = kaiming_init                 
+        elif mode == 'normal':                         
+            initializer = normal_init                  
+                                                       
+        for block in self._modules:                    
+            for m in self._modules[block]:             
+                initializer(m)                         
+                                                       
+    def reparametrize(self, mu, logvar):               
+        std = logvar.mul(0.5).exp_()
+        #print(std.shape)                 
+        eps = std.data.new(std.size()).normal_()
+        return eps.mul(std).add_(mu)                   
+                                                       
+    def forward(self, x, no_dec=False):                
+        stats = self.encode(x)
+        print("stats")
+        print(stats.shape)
+        quit()
+        mu = stats[:, :self.z_dim]                     
+        logvar = stats[:, self.z_dim:]                 
+        z = self.reparametrize(mu, logvar)
+        if no_dec:                                     
+            return z.squeeze()                         
+        else:                                          
+            x_recon = self.decode(z)                   
+
+class Custom_FactorVAE2(nn.Module):
+    def __init__(self, z_dim=10):
+        super(Custom_FactorVAE2, self).__init__()
+        self.z_dim = z_dim
+        self.dense_enc1 = nn.Linear(28*28, 200)
+        self.dense_enc2 = nn.Linear(200, 100)
+        self.dense_encmean = nn.Linear(100, z_dim)
+        self.dense_encvar = nn.Linear(100, z_dim)
+        self.dense_dec1 = nn.Linear(z_dim, 100)
+        self.dense_dec2 = nn.Linear(100, 200)
+        self.dense_dec3 = nn.Linear(200, 28*28)
+        #self.weight_init()
+
+    def weight_init(self, mode='normal'):
+        if mode == 'kaiming':
+            initializer = kaiming_init
+        elif mode == 'normal':
+            initializer = normal_init
+
+        for block in self._modules:
+            for m in self._modules[block]:
+                initializer(m)
+        
+    def _encoder(self, x):
+        x = F.relu(self.dense_enc1(x))
+        x = F.relu(self.dense_enc2(x))
+        mean = self.dense_encmean(x)
+        var = self.dense_encvar(x)
+        #var = F.softplus(self.dense_encvar(x))
+        return mean, var
+    
+    def _sample_z(self, mean, var): #普通にやると誤差逆伝搬ができないのでReparameterization Trickを活用
+        device = 'cuda'
+        epsilon = torch.randn(mean.shape).to(device)
+        #return mean + torch.sqrt(var) * epsilon #平均 + episilonは正規分布に従う乱数, torc.sqrtは分散とみなす？平均のルート
+        return mean + epsilon * torch.exp(0.5*var)
+        # イメージとしては正規分布の中からランダムにデータを取り出している
+        #入力に対して潜在空間上で類似したデータを復元できるように学習, 潜在変数を変化させると類似したデータを生成
+        #Autoencoderは決定論的入力と同じものを復元しようとする
+
+
+    def _decoder(self,z):
+        x = F.relu(self.dense_dec1(z))
+        x = F.relu(self.dense_dec2(x))
+        x = F.sigmoid(self.dense_dec3(x))
+        return x
+
+    def forward(self, x, no_dec=False):
+        x = x.view(x.shape[0], -1)
+        mean, var = self._encoder(x)
+        z = self._sample_z(mean, var)
+        if no_dec:
+            return z.squeeze()
+        else:
+            x_recon = self._decoder(z)
+            return x_recon, mean, var, z.squeeze()
+
+def kaiming_init(m):
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        init.kaiming_normal_(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+        m.weight.data.fill_(1)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+
+def normal_init(m):
+    if isinstance(m, (nn.Linear, nn.Conv2d)):
+        init.normal_(m.weight, 0, 0.02)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+        m.weight.data.fill_(1)
+        if m.bias is not None:
+            m.bias.data.fill_(0)
